2da4bdd7a85cab0056cc4f297b7434c82fb70eb5
yuanqh23 <3157085660@qq.com>
2025-07-31 23:34:52 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: [
+      'devserver-main--meek-marshmallow-d320f9.netlify.app'
+    ]
+  },
   plugins: [vue()],
   mode: 'production'
 })
